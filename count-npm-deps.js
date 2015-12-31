a29b360bb11799f465088c8dd2f6d8cfcf346d4e
jack <jack@wanjienideMacBook-Air.local>
2015-12-31 14:02:54 +0800

    add main script

---
+++ count-npm-deps.js
@@ -0,0 +1,39 @@
+'use strict'
+
+let exec = require('child_process').exec
+                                                                            ;(new Promise((resolve, reject) =>
+                                                                              // find all subdirs named "node_modules"
+  exec('find . -name node_modules -type d', (err, stdout, stderr) =>
+    resolve(stdout.split('\n'))
+  )
+)).then(nodeModulesDirs => {
+  return Promise.all(nodeModulesDirs.map(dir =>
+    new Promise((resolve, reject) => {
+      // list one-level subdirs except hidden ones
+      const cmd = `find ${dir} -depth 1 -type d ! -iname ".*"`
+      exec(cmd, (err, stdout, stderr) => {
+        // remove trailing empty new line first
+        let pathsStr = stdout.replace(/\n\s*$/, '')
+        resolve(pathsStr.split('\n'))
+        console.log(pathsStr)
+      })
+    })
+  )).then(depsList => {
+    console.log('-'.repeat(30))
+    let deps = depsList.reduce(
+      (result, deps) => {
+        result.push(...deps)
+        return result
+      },
+      []
+    )
+    let totalDepsCount = deps.length
+    console.log(`total deps count: ${totalDepsCount}`)
+
+    let topDeps = deps.filter(
+      dep => !/(node_modules).+\1/.test(dep)
+    )
+    let topDepsCount = topDeps.length
+    console.log(`top deps count: ${topDepsCount}`)
+  })
+})
